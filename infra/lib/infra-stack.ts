fa9afc8389fc2f5d9a406bafa63b05271e54dbbe
nt54hamnghi <hamnghi250699@gmail.com>
2023-03-28 12:05:33 -0400

    complete cdk stack

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,55 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
-export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+function genid(service: string) {
+	const Service = service.charAt(0).toUpperCase() + service.slice(1);
+	return `Burudoza${Service}`;
 }
+
+export class BurudozaStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		const vpc = new ec2.Vpc(this, genid('vpc'), {
+			maxAzs: 2, // maximum number of availability zone, default is All
+		});
+
+		const cluster = new ecs.Cluster(this, genid('cluster'), {
+			vpc: vpc,
+		});
+
+		const asset = new DockerImageAsset(this, genid('dockerAsset'), {
+			directory: join(__dirname, '../..'),
+		});
+
+		const image = ecs.ContainerImage.fromDockerImageAsset(asset);
+
+		new ecsPatterns.ApplicationLoadBalancedFargateService(
+			this,
+			genid('fargateService'),
+			{
+				assignPublicIp: true,
+				cluster: cluster,
+				cpu: 2048, // 2 vCPU
+				taskImageOptions: {
+					image: image,
+					containerPort: 8501,
+				},
+				memoryLimitMiB: 4096, // 4 BG of RAM
+				publicLoadBalancer: true,
+			}
+		);
+	}
+}
+
+// import * as ecr from 'aws-cdk-lib/aws-ecr';
+// const repo = ecr.Repository.fromRepositoryName(
+// 	this,
+// 	genid('repository'),
+// 	'burudoza'
+// );
