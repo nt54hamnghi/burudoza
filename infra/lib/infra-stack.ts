b4cd219d70b80d96e6e755e0444a0348097c583d
nt54hamnghi <hamnghi250699@gmail.com>
2023-04-10 15:29:21 -0400

    remove nat gateway

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -17,14 +17,6 @@ export class BurudozaStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const vpc = new ec2.Vpc(this, genid('vpc'), {
-			maxAzs: 2, // maximum number of availability zone, default is All
-		});
-
-		const cluster = new ecs.Cluster(this, genid('cluster'), {
-			vpc: vpc,
-		});
-
 		// Set up the hosted zone
 		const domainName = 'burudoza.com';
 		// Import an existing hosted Zone
@@ -47,13 +39,17 @@ export class BurudozaStack extends cdk.Stack {
 
 		const image = ecs.ContainerImage.fromDockerImageAsset(asset);
 
-		// const capacityProviderStrategy: ecs.CapacityProviderStrategy =
+		const vpc = new ec2.Vpc(this, genid('vpc'), {
+			maxAzs: 2, // maximum number of availability zone
+			natGateways: 0,
+		});
+
 		new ecsPatterns.ApplicationLoadBalancedFargateService(
 			this,
 			genid('fargateService'),
 			{
-				assignPublicIp: true,
-				cluster,
+				vpc,
+				assignPublicIp: true, // whether the service will be assigned a public IP address.
 				capacityProviderStrategies: [
 					{
 						capacityProvider: 'FARGATE_SPOT',
@@ -66,15 +62,16 @@ export class BurudozaStack extends cdk.Stack {
 						weight: 1,
 					},
 				],
-				cpu: 2048, // 4 vCPU
 				taskImageOptions: {
 					image,
 					containerPort: 8501,
 				},
+				cpu: 2048, // 2 vCPU
 				memoryLimitMiB: 4096, // 4 BG of RAM
-				publicLoadBalancer: true,
-				certificate: certificate,
+				publicLoadBalancer: true, // whether the Load Balancer will be internet-facing.
+				certificate,
 				listenerPort: 443, // default 80, 433 is for HTTPS
+				redirectHTTP: true, // automatically create a listener on port 80 that redirects HTTP traffic to the HTTPS port.
 				domainName: `app.${domainName}`,
 				domainZone: hostedZone,
 			}
